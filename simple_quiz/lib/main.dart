b941b1250579472f7d17e08476ad73f596b84835
Biggo <routh.biggo123@gmail.com>
2022-06-06 00:21:51 +0545

    simple_quiz

---
+++ simple_quiz/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import './Question.dart';
+import 'answer.dart';
+import './quiz.dart';
+import './result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Black', 'Red', 'Green', 'Blue'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favourite hero?',
+      'answers': ['Cole', 'Tom', 'Depp', 'Decaprio'],
+    },
+  ];
+
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
+    );
+  }
+}
