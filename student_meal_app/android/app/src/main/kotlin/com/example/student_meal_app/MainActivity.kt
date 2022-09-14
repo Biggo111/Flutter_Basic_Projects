ff297d956569a6a756f56326c8e1e41ae0bb0f43
Biggo Routh <biggo@Biggos-MacBook-Air.local>
2022-09-15 00:53:23 +0600

    Student meal basic project

---
+++ student_meal_app/android/app/src/main/kotlin/com/example/student_meal_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.student_meal_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ student_meal_app/lib/categories_screens.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_Item.dart';
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Student Meal System"),
+        centerTitle: true,
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 300,
+          childAspectRatio: 3/2,
+          mainAxisSpacing: 20,
+        ) 
+      ),
+    );
+  }
+}
